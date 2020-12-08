7c93d6e0f9aa32f217309c53355fe63bfccb4919
VladTzaru <bvanja.87@gmail.com>
2020-12-08 21:16:16 +0100

    Added react-bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 // Custom imports - COMPONENTS
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <main>
-        <h1>Hi there</h1>
+        <Container>
+          <h1>Hi there</h1>
+        </Container>
       </main>
       <Footer />
     </>
