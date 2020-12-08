71b889380658f731d68fa620014be413bf07b48a
VladTzaru <bvanja.87@gmail.com>
2020-12-08 21:07:32 +0100

    Added Header and Footer components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+// Custom imports - COMPONENTS
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hi there</h1>
+      <Header />
+      <main>
+        <h1>Hi there</h1>
+      </main>
+      <Footer />
     </>
   );
 };
