18d34f69082e97c612d5e1ae8bfd9277fe71221c
VladTzaru <bvanja.87@gmail.com>
2020-12-09 22:13:14 +0100

    Added product routes: all products and a single one

--- server/server.js
+++ server/server.js
@@ -1,21 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+// Import Routes
+import productRoutes from './routes/product.js';
 
 dotenv.config();
 connectDB();
-
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// Mount routes
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
