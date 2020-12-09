ba9a474f99d7fb01066b62005cbcc43c2ffb049e
VladTzaru <bvanja.87@gmail.com>
2020-12-09 17:13:52 +0100

    Added env. variables

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 const app = express();
 
 app.get('/api/products', (req, res) => {
@@ -11,4 +14,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('SERVER RUNNING ON PORT 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`SERVER RUNNING IN ${process.env.NODE_ENV} MODE ON PORT ${PORT}`)
+);
