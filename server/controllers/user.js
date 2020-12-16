bbdb9730eae3faadcac5d0029bc56ca602949352
VladTzaru <bvanja.87@gmail.com>
2020-12-16 15:27:51 +0100

    Added authUser controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,12 +1,27 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
 
 // @desc     Auth user and get token
 // @route    GET /api/users/login
 // @access   Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  res.json({ email, password });
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password.');
+  }
 });
 
 export { authUser };
