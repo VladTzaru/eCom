3d7fba7f307395a6c8ce2e43e996ce880c7a61c5
VladTzaru <bvanja.87@gmail.com>
2020-12-17 16:12:31 +0100

    Call login in LoginPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,14 +1,22 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Formik, Field, Form } from 'formik';
-import React from 'react';
-import { Button, FormGroup } from 'react-bootstrap';
+import { Button, Col, FormGroup, Row } from 'react-bootstrap';
 import FormContainer from '../components/Form/FormContainer';
 import * as Yup from 'yup';
 import FormInput from '../components/Form/FormInput';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { RootStore } from '../redux/store';
+import { login } from '../redux/actions/user/user';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 interface Values {
   email: string;
   password: string;
 }
+
+interface LoginPageProps extends RouteComponentProps {}
 
 const initialValues: Values = {
   email: '',
@@ -20,14 +28,32 @@ const validationSchema = Yup.object({
   password: Yup.string().required().min(6),
 });
 
-const LoginPage = () => {
+const LoginPage: React.FC<LoginPageProps> = ({ location, history }) => {
+  const dispatch = useDispatch();
+  const { userDetails, loading, error } = useSelector(
+    (state: RootStore) => state.user
+  );
+
+  useEffect(() => {
+    if (userDetails) history.push('/');
+  }, [history, userDetails]);
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values) => console.log(values)}
-    >
-      <FormContainer>
+    <FormContainer>
+      <h4 className='mb-4'>Login to your account</h4>
+      {error && (
+        <Message visible={true} variant='danger'>
+          {error}
+        </Message>
+      )}
+      {loading && <Loader />}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={({ email, password }) => {
+          dispatch(login(email, password));
+        }}
+      >
         <Form>
           <FormGroup>
             <Field
@@ -54,8 +80,14 @@ const LoginPage = () => {
             </Button>
           </FormGroup>
         </Form>
-      </FormContainer>
-    </Formik>
+      </Formik>
+
+      <Row className='py-3'>
+        <Col>
+          <span>New Customer?</span> <Link to='/register'>Register</Link>
+        </Col>
+      </Row>
+    </FormContainer>
   );
 };
 
