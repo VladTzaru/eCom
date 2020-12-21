73bf5d25e5f01f4a8da7a54d0463bb14875a5958
VladTzaru <bvanja.87@gmail.com>
2020-12-21 19:52:00 +0100

    Added deleteUser controller and route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -131,10 +131,25 @@ const getAllUsers = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc     Delete a user
+// @route    DELETE /api/users/:id
+// @access   Private (Admin)
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.remove();
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
   registerUser,
   updateUserProfile,
   getAllUsers,
+  deleteUser,
 };
